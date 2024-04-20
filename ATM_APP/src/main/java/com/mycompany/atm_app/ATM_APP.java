c96e87efd9348407816e182640d969a1306136d4
Omar Hussein <117474007+Eng-Omar-Hussein@users.noreply.github.com>
2024-04-20 21:24:59 +0200

    Add files via upload

---
+++ ATM_APP/src/main/java/com/mycompany/atm_app/ATM_APP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atm_app;
+
+/**
+ *
+ * @author pc2
+ */
+public class ATM_APP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Main obj = new Main();
+    }
+}
